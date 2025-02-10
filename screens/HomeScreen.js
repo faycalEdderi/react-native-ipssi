6fc0d60c7e31885e6a39b4c8e83e8249a0cbf1a2
faycalEdderi <faycmx@live.fr>
2025-02-10 13:03:38 +0100

    init project navigaton

---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Bienvenue sur l'écran d'accueil !</Text>
+      <Button
+        title="Aller au Profil"
+        onPress={() => navigation.navigate("Profile")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  text: { fontSize: 18, marginBottom: 20 },
+});
+
+export default HomeScreen;
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const ProfileScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Bienvenue sur votre profil !</Text>
+      <Button title="Retour" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  text: { fontSize: 18, marginBottom: 20 },
+});
+
+export default ProfileScreen;
